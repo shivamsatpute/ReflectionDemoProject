245386068340acbdad14ee894473a3cbddd6d752
shivam satpute <shivam897satpute@gmail.com>
2021-06-01 16:55:01 +0530

    [shivam]add: Code to implement Reflection,Annotation and Garbage collection

---
+++ AnnotationsDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnnotationsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Annotations Demo");
+            AnnotationsTest.Display();
+        }
+    }
+}
+
